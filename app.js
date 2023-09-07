6c89f59494e1cacb5b0025173358fbccbe94f51f
Lakshya Kumar <135580752+its-lakshya@users.noreply.github.com>
2023-09-07 09:49:35 +0530

    added the filter through search functionality with stimulusJS

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const application = Stimulus.Application.start();
+
+application.register("table", class extends Stimulus.Controller {
+  static targets = ["row"]
+
+
+  filter(event) {
+    const query = event.target.value.toLowerCase();
+    console.log()
+    this.rowTargets.forEach((row) => {
+      const text = row.textContent.toLowerCase();
+      if (text.includes(query)) {
+        row.style.display = "";
+      } else {
+        row.style.display = "none";
+      }
+    });
+  }
+
+  get queryValue() {
+    return this.query || "";
+  }
+});
